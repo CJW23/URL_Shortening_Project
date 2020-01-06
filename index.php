42e27bed06ec40292ef5bed5f7edbd697e5bb38e
cjw7242 <cjw7242@gmail.com>
2020-01-06 17:15:24 +0900

    not found 404 처리 테스트

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+    
+    awdawdawd
+    <?php
+        header('Location:http://naver.com}');
+        echo "awd";
+        header('www.naver.com');
+    ?>
+    
+</body>
+</html>
